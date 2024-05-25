eb573ac30c48dac6f917121900966f2d62f145f5
haxerman1 <121405620+haxerman1@users.noreply.github.com>
2024-05-26 00:06:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ window.onload = function() {
         }, 5000);
     }, 100);
 };
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '0',
+        width: '0',
+        videoId: 'DsY_kocbWaM',
+        events: {
+            'onReady': onPlayerReady,
+        },
+        playerVars: {
+            'autoplay': 1,
+            'controls': 0,
+            'showinfo': 0,
+            'rel': 0,
+            'loop': 1,
+            'playlist': 'DsY_kocbWaM'
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+    event.target.setVolume(50); // Set volume level to 50%
+}
+
